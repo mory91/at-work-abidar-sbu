6a3dde83cadf32b8ae8614ee5a2d9f612b84744b
Erfan <Erfan>
2017-03-27 20:01:23 +0430

    QR code reader added.

--- at-work-abidar-sbu/QrCodeTestForm.cs
+++ at-work-abidar-sbu/QrCodeTestForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing;
 
 namespace at_work_abidar_sbu
 {
@@ -16,5 +17,35 @@ namespace at_work_abidar_sbu
         {
             InitializeComponent();
         }
-    }
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			// create a barcode reader instance
+			IBarcodeReader reader = new BarcodeReader();
+			// load a bitmap
+			var barcodeBitmap = (Bitmap)Bitmap.FromFile(textBox1.Text);
+			// detect and decode the barcode inside the bitmap
+			var result = reader.Decode(barcodeBitmap);
+			// do something with the result
+			if (result != null)
+			{
+				textBox3.Text = result.BarcodeFormat.ToString();
+				textBox2.Text = result.Text;
+			}
+			else
+			{
+				textBox3.Text = ":|";
+				textBox2.Text = ":|";
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				textBox1.Text = openFileDialog1.FileName;
+				pictureBox1.Load(textBox1.Text);
+			}
+		}
+	}
 }
